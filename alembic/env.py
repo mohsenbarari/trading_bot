24e7a856c30518b8796e0fd4f2770a54192daf5e
mohsenbarari <mmchavoshi235@gmail.com>
2025-09-28 16:53:26 +0300

    fix error

--- alembic/env.py
+++ alembic/env.py
@@ -1,28 +1,30 @@
-# alembic/env.py (نسخه نهایی و همزمان برای manage.py)
+
 from logging.config import fileConfig
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+
 from alembic import context
-import os
 
-# آبجکت Config که به تنظیمات alembic.ini دسترسی دارد
-config = context.config
 
-# این بخش ضروری نیست چون ما آدرس را در manage.py تنظیم می‌کنیم، اما بودنش ضرری ندارد
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-# مدل‌های SQLAlchemy خود را برای پشتیبانی از autogenerate وارد کنید
 from models.database import Base
 import models.user
 import models.invitation
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
 target_metadata = Base.metadata
+
 
 def run_migrations_online() -> None:
     """اجرای migration ها در حالت آنلاین به صورت همزمان."""
-    # ایجاد یک engine با استفاده از تنظیماتی که manage.py به ما می‌دهد
+
     connectable = engine_from_config(
-        config.get_section(config.main_section_name, {}),
+        config.get_section(config.config_ini_section), 
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -31,8 +33,9 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
+
         with context.begin_transaction():
             context.run_migrations()
 
-# ما فقط حالت آنلاین را نیاز داریم
+
 run_migrations_online()
--- core/config.py
+++ core/config.py
@@ -1,9 +1,10 @@
+
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     bot_token: str
     database_url: str
-    #sync_database_url: str
+    sync_database_url: str  
     postgres_db: str
     postgres_user: str
     postgres_password: str
